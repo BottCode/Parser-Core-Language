0e241b4bea32bfae3548517449ac7caf96f0cac8
BottCode <mattiabottaro9@gmail.com>
2018-04-04 23:35:41 +0200

    first part almost done

--- read.hs
+++ read.hs
@@ -1,25 +1,28 @@
-import System.IO
 import ParserCoreLanguage
-
-readF :: IO String
-readF = do inh <- OpenFile "input.txt" ReadMode
-           prog <- readloop inh
-           hClose inh
-           return prog 
+import System.IO
 
 main :: IO (Program Name)
 main = do inp <- readF
           return (comp (parse parseProg inp)) -- here you call parseProg
+
+readF :: IO String
+readF = do inh <- openFile "input.txt" ReadMode
+           prog <- readloop inh
+           hClose inh
+           return prog 
           
 comp :: [(Program Name,Name)] -> Program Name
 comp [] = error "no parse"
 comp [(e,[])] = e
 comp [(_,a)] = error ("does't use all input"++a)
 
-readloop inh = do ineof <- hIsEof inh
+readloop inh = do ineof <- hIsEOF inh
                   if ineof
                        then return []
                        else do
                              x <- hGetLine inh
                              xs <- readloop inh
-                             retun (x ++ xs)  
+                             return (x ++ xs)  
+
+script = do inp <- readF
+            return (parse parseProg inp)
